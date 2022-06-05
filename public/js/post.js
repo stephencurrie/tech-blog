ff980538e182925eabf914e1b0ff218df49cb0aa
scurrie <99137578+stephencurrie@users.noreply.github.com>
2022-06-05 18:10:14 -0400

    added handlebars for blog, create and edit

---
+++ public/js/post.js
@@ -0,0 +1,19 @@
+const newPostHandler = async (evt) => {
+    evt.preventDefault();
+    const title = document.querySelector('#create-post-title').value;
+    const content = document.querySelector('#create-post-content').value;
+  
+    await fetch('/api/posts', {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        content,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+  };
+  
+  document
+    .querySelector('#create-post-form')
+    .addEventListener('submit', newPostHandler);
